c55ebc713e80c9492ae5010b260fd51a4820539a
Daniel <danielchacon1699@gmail.com>
2023-01-07 18:37:27 -0600

    resume updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
-import resume from '../assets/resume.pdf'
+import DanielChacon_resume from '../assets/DanielChacon_resume.pdf'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -121,8 +121,8 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#487C74]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              download="resume"
-              href={resume}
+              download="DanielChacon_resume"
+              href={DanielChacon_resume}
             >
               Resume
               <BsFillPersonLinesFill size={30} />
